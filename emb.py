30447c734a51f99d9a59cb98fa464ea4b8b87978
dzetfunc <dzetfunc@gmail.com>
2019-03-01 13:24:22 +0300

    Embeddings on WMT; l2 and cosine loss

---
+++ emb.py
@@ -0,0 +1,18 @@
+from gensim.models.word2vec import LineSentence
+from gensim.models.word2vec import Word2Vec
+from gensim.models.fasttext import FastText
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--corpus-path', required=True)
+parser.add_argument('--model-path', required=True)
+parser.add_argument('--num-workers', default=20, type=int)
+parser.add_argument('--model-type', choices=['w2v', 'fasttext'], required=True)
+args = parser.parse_args()
+
+if args.model_type == 'w2v':
+    model_corp_file = Word2Vec(corpus_file=args.corpus_path, iter=5, size=300, workers=args.num_workers)
+else:
+    model_corp_file = FastText(corpus_file=args.corpus_path, iter=5, size=300, workers=args.num_workers)
+model_corp_file.wv.save_word2vec_format(args.model_path)
